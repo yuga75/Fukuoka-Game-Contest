ea5aa8d6ffa28edb50a97988d8d0d0438fa35d3b
pregon7 <BS221035@sun.ac.jp>
2022-10-28 11:15:25 +0900

    ギミックの画像切り替え機能の追加
    ギミックの画像切り替え機能を追加しました。

---
+++ Assets/Scripts/ChangeImage.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour
+{
+    [SerializeField] private Sprite OnImage;
+    [SerializeField] private Sprite OffImage;
+    SpriteRenderer sr;
+    private bool ImageFlag = false;
+
+    void Start(){
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void OnClickChangeImage(){
+        if(ImageFlag == false){
+            sr.sprite = OnImage;
+            ImageFlag = true;
+            Debug.Log ("On");
+        }
+
+        else if(ImageFlag == true){
+            sr.sprite = OffImage;
+            ImageFlag = false;
+            Debug.Log ("Off");
+        }
+    }
+}
