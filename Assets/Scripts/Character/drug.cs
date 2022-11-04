a7c76bb7e9751804ec7ca2463ff2a9abcbd8c1a1
野中宥我 <95630808+yuga75@users.noreply.github.com>
2022-11-04 15:15:57 +0900

    プレイヤー用のドラッグ&ドロップのスクリプトを作成

---
+++ Assets/Scripts/Character/drug.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Vector2 prePosition;
+
+    /// <summary>
+    /// ドラッグ開始時に呼び出される
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        prePosition = transform.position;
+    }
+
+    /// <summary>
+    /// ドラッグ中に呼び出される
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    /// <summary>
+    /// ドラッグ終わりに呼び出される
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        bool flg = true;
+
+        var raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raycastResults);
+
+        foreach (var hit in raycastResults)
+        {
+            if (hit.gameObject.CompareTag("hogehoge"))
+            {
+                transform.position = hit.gameObject.transform.position;
+                flg = false;
+            }
+        }
+
+        if (flg)
+        {
+            transform.position = prePosition;
+        }
+    }
+
+}
